5e5cf8ca081bd35d1e874c99a74d737bc0d373c7
AAYUSH PRAJAPATI <aayushp1803@gmail.com>
2025-12-15 11:18:17 +0530

    This Website Is Created Fully Responsive With HTML CSS BOOTSTRAP...

---
+++ js/Script.js
@@ -0,0 +1,53 @@
+const toggleBtn = document.getElementById("toggleBtn");
+const navMenu = document.getElementById("navMenu");
+
+toggleBtn.onclick = () => {
+  navMenu.classList.toggle("active");
+};
+
+var swiper = new Swiper(".mySwiper", {
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+});
+
+
+ var swiper = new Swiper(".myswiper", {
+    slidesPerView: 3,
+    spaceBetween: 20,
+    loop: true,
+    autoplay: {
+        delay: 2000,
+        disableOnInteraction: false,
+    },
+    // navigation: {
+    //     nextEl: ".swiper-button-next",
+    //     prevEl: ".swiper-button-prev",
+    // },
+
+    breakpoints: {
+        0: {
+            slidesPerView: 1
+        },
+        576: {
+            slidesPerView: 2
+        },
+        992: {
+            slidesPerView: 3
+        }
+    }
+});
+
+
+
+  
